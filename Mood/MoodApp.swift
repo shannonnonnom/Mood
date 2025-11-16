25d8bd6b42e69ba0b3df915ffcfdb5205335f084
zih syuan <ziisyuan@gmail.com>
2025-11-16 10:00:54 +0800

    Initial Commit

---
+++ Mood/MoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodApp.swift
+//  Mood
+//
+//  Created by Zih Syuan Kuo on 2025/11/16.
+//
+
+import SwiftUI
+
+@main
+struct MoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
